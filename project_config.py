deef9cd4e894a927c587402dfdcb9f887bff5a05
Christoph Franke <christophfranke123@googlemail.com>
2017-01-05 17:26:35 +0100

    added project config

---
+++ project_config.py
@@ -0,0 +1,11 @@
+
+LOCAL_DOMAIN = 'fabiankampa.local'
+LIVE_DOMAIN = 'relaunch.fabiankampa.de'
+
+KIS_PASSWORD = '..V4UubH1XoeOUmxsT44oL'
+KIS_LOGIN = 'fabiankampa'
+
+
+FTP_USER = 'ftp10975786-krito'
+FTP_PASSWORD = 'zCU5rQJrSkBsU17eJA09'
+FTP_PATH_TO_WWW_DIR = '.'
