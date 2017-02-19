3227711e8df4784b46bc0b661d791abefe6e91be
Christoph Franke <christophfranke123@googlemail.com>
2017-02-19 13:34:05 +0100

    bugfix html entity

--- www/site/plugins/umlaut-entities/umlaut-entities.php
+++ www/site/plugins/umlaut-entities/umlaut-entities.php
@@ -7,6 +7,6 @@ field::$methods['kirbytext'] = function($field) {
 	$field->value = str_replace('Ä', '&Auml;', $field->value);
 	$field->value = str_replace('Ö', '&Ouml;', $field->value);
 	$field->value = str_replace('Ü', '&Uuml;', $field->value);
-	$field->value = str_replace('ß', '&szlg;', $field->value);
+	$field->value = str_replace('ß', '&szlig;', $field->value);
 	return kirbytext($field);
 };
